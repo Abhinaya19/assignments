03bb3eb0f0bb18586da68a3d7fccc62ada851ea1
Abhinaya19 <abhinayaravi19@gmail.com>
2016-01-18 18:20:44 +0530

    Create magicsquares.c

---
+++ magicsquares.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+ 
+int main( )
+{
+    int size;
+    int a[50][50]={0};
+    printf("Enter the size 0f odd number: ");
+    scanf("%d", &size);
+    int sqr = size * size;
+    int i = 0, j = size / 2, k;
+    for (k = 1; k <= sqr;k++) 
+    {
+        if(a[i][j] == 0)
+        {
+            a[i][j]=k;
+       }
+        else if(a[i][j]!=0) 
+        { 
+          i=i+2;
+          j--;
+          a[i][j]=k;
+       }
+      i--;
+      j++;
+      if(k%2==0)
+      {
+          i=i+2;
+          j--;
+      }
+      if (j == size) 
+      {
+         j -= size;
+     }
+     if (i < 0)
+    {
+        i += size;
+    }
+  }
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            printf("%d  ", a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
